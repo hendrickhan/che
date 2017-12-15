916329a74befdad54f2ce0c460a6d9dfaf7ad773
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-15 22:14:44 +0200

    Added jprofiler installer

---
+++ agents/j-profiler/src/main/resources/installers/1.0.1/org.eclipse.che.jprofiler.script.sh
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2012-2017 Red Hat, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+
+wget https://download-keycdn.ej-technologies.com/jprofiler/jprofiler_linux_10_0_4.tar.gz -P /tmp/
+tar -xzf /tmp/jprofiler_linux_10_0_4.tar.gz -C ~/
+rm /tmp/jprofiler_linux_10_0_4.tar.gz
