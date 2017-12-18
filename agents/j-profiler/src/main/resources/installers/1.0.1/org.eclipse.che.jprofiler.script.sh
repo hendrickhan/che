0d59a370e5c2da0eaa2c9e4f97eb3ed04362b9ff
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-18 10:58:08 +0200

    fixup! Added jprofiler installer

--- agents/j-profiler/src/main/resources/installers/1.0.1/org.eclipse.che.jprofiler.script.sh
+++ agents/j-profiler/src/main/resources/installers/1.0.1/org.eclipse.che.jprofiler.script.sh
@@ -8,7 +8,10 @@
 # Contributors:
 #   Red Hat, Inc. - initial API and implementation
 #
-
+echo "j1"
 wget https://download-keycdn.ej-technologies.com/jprofiler/jprofiler_linux_10_0_4.tar.gz -P /tmp/
+echo "j2"
 tar -xzf /tmp/jprofiler_linux_10_0_4.tar.gz -C ~/
+echo "j3"
 rm /tmp/jprofiler_linux_10_0_4.tar.gz
+echo "j4"
